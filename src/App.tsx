78c1c6e9dfc7702753def8367341afc04c69caba
Mehdi Allahyari <mehdi.allahyari@gmail.com>
2023-03-28 08:52:27 -0400

    chatinterface udpated.

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,15 @@ import FileUploader from './pages/fileUploader';
 import ChatInterface from './components/chat/ChatInterface';
 
 function App() {
-  return (
-    <Container>
-      <Box mt={3} display="flex" flexDirection="column" alignItems="center">
-        <Typography variant="h4">Ask me anything about your data</Typography>
-      </Box>
-      <FileUploader />
-      <ChatInterface />
-    </Container>
-  );
+	return (
+		<div className="main-holder">
+			<Box mt={3} display="flex" flexDirection="column" alignItems="center">
+				<Typography variant="h4">Ask me anything about your data</Typography>
+			</Box>
+			<FileUploader />
+			<ChatInterface />
+		</div>
+	);
 }
 
 export default App;
--- src/components/chat/ChatInterface.tsx
+++ src/components/chat/ChatInterface.tsx
@@ -1,88 +1,101 @@
-import React, { useState } from 'react';
-import { Button, Box, Grid, Avatar, Typography } from '@mui/material';
-
+import React, { useState } from "react";
+import { Button, Box, Grid, Avatar, Typography } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
 interface ChatMessage {
-  id: string;
-  message: string;
-  isUser: boolean;
-  avatar: string;
+	id: string;
+	message: string;
+	isUser: boolean;
+	avatar: string;
 }
 
 const ChatInterface: React.FC = () => {
-  const [messages, setMessages] = useState<ChatMessage[]>([]);
-  const [inputMessage, setInputMessage] = useState('');
+	const [messages, setMessages] = useState<ChatMessage[]>([]);
+	const [inputMessage, setInputMessage] = useState("");
 
-  const handleInputMessageChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setInputMessage(event.target.value);
-  };
+	const handleInputMessageChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+		setInputMessage(event.target.value);
+	};
 
-  const handleInputMessageSubmit = (
-    event: React.FormEvent<HTMLFormElement>
-  ) => {
-    event.preventDefault();
+	const handleKeyDown = (event: React.KeyboardEvent) => {
+		if (event.key === "Enter") {
+			event.preventDefault(); // Prevents a new line from being added
+			handleInputMessageSubmit();
+		}
+	};
 
-    if (inputMessage.trim() === '') {
-      return;
-    }
+	const handleInputMessageSubmit = () => {
+		if (inputMessage.trim() === "") {
+			return;
+		}
 
-    const newMessage: ChatMessage = {
-      id: Math.random().toString(36).substring(7),
-      message: inputMessage,
-      isUser: true,
-      avatar: 'https://example.com/user-avatar.png',
-    };
+		const newMessage: ChatMessage = {
+			id: Math.random().toString(36).substring(7),
+			message: inputMessage,
+			isUser: true,
+			avatar: "https://example.com/user-avatar.png",
+		};
 
-    setMessages([
-      ...messages,
-      newMessage,
-      {
-        id: Math.random().toString(36).substring(7),
-        message: 'Loading...',
-        isUser: false,
-        avatar: 'https://example.com/chatbot-avatar.png',
-      },
-    ]);
+		setMessages([
+			...messages,
+			newMessage,
+			{
+				id: Math.random().toString(36).substring(7),
+				message: "Loading...",
+				isUser: false,
+				avatar: "https://example.com/chatbot-avatar.png",
+			},
+		]);
 
-    // Here you can send the inputMessage to the server to get the response
-    // Once the response is received, you can update the messages array with the response
+		// Here you can send the inputMessage to the server to get the response
+		// Once the response is received, you can update the messages array with the response
 
-    // For this example, we'll just simulate a delay before showing the response
-    setTimeout(() => {
-      const chatbotMessage: ChatMessage = {
-        id: Math.random().toString(36).substring(7),
-        message: 'This is a response from the chatbot.',
-        isUser: false,
-        avatar: 'https://example.com/chatbot-avatar.png',
-      };
+		// For this example, we'll just simulate a delay before showing the response
+		setTimeout(() => {
+			const chatbotMessage: ChatMessage = {
+				id: Math.random().toString(36).substring(7),
+				message: "This is a response from the chatbot.",
+				isUser: false,
+				avatar: "https://example.com/chatbot-avatar.png",
+			};
 
-      setMessages([...messages.slice(0, -1), newMessage, chatbotMessage]);
-    }, 1000);
+			setMessages([...messages.slice(0, -1), newMessage, chatbotMessage]);
+		}, 1000);
 
-    setInputMessage('');
-  };
+		setInputMessage("");
+	};
 
-  return (
-    <Box>
-      <div>
-        {messages.map((message) => (
-          <div key={message.id}>
-            <img src={message.avatar} alt="Avatar" />
-            {message.message}
-          </div>
-        ))}
-      </div>
-      <form onSubmit={handleInputMessageSubmit}>
-        <input
+	return (
+		<Box display='flex' height="100%">
+			<div className="chatbox">
+				<div className="chat-logs">
+					{messages.map((message) => (
+						<div key={message.id}>
+							<img src={message.avatar} alt="Avatar" />
+							{message.message}
+						</div>
+					))}
+				</div>
+				<div className="chat-input-holder">
+					<textarea
+						className="chat-input"
+						rows={1}
+						onKeyDown={handleKeyDown}
+						onChange={handleInputMessageChange}
+					></textarea>
+
+					{/* <input
           type="text"
           value={inputMessage}
           onChange={handleInputMessageChange}
-        />
-        <button type="submit">Send</button>
-      </form>
-    </Box>
-  );
+        /> */}
+					{/* <button className="chat-input-btn">
+					<i className="fa fa-paper-plane chat-input-send-icon" aria-hidden="true"></i>
+					Send
+				</button> */}
+				</div>
+			</div>
+		</Box>
+	);
 };
 
 export default ChatInterface;
